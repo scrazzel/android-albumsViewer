aa47fc6973da9f08aeb601911bb611263d86b8fe
Rafał Zawadzki <rafal.zawadzki@savangard.com>
2018-06-06 18:25:12 +0200

    Albums not working

--- app/src/main/java/com/example/admin/albumsviewer/AlbumsList.java
+++ app/src/main/java/com/example/admin/albumsviewer/AlbumsList.java
@@ -1,19 +1,39 @@
 package com.example.admin.albumsviewer;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 import android.support.v7.widget.Toolbar;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AlbumsList extends AppCompatActivity {
 
     Toolbar toolbar;
+
+    DatabaseReference database;
+
+    ListView albumsList;
+
+    ArrayList<String> arrayList = new ArrayList<>();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +43,42 @@ public class AlbumsList extends AppCompatActivity {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         //toolbar.setTitle("Biblioteka albumów");
         setSupportActionBar(toolbar);
+
+        database = FirebaseDatabase.getInstance().getReference();
+        albumsList = (ListView)findViewById(R.id.albumsList);
+
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(AlbumsList.this,android.R.layout.simple_list_item_1, arrayList);
+
+        albumsList.setAdapter(arrayAdapter);
+
+        database.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                String value = dataSnapshot.getValue(String.class);
+                arrayList.add(value);
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
     @Override
